e99f3dc778a5ec84dd4cd7b2ddd9823e934cd7dc
Jamal Hasuev <jhasuev@mail.ru>
2024-01-12 13:46:00 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TextField />
+  </div>
+</template>
+
+<script>
+import TextField from './components/TextField.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TextField,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/TextField.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    field
+  </div>
+</template>
+
+<script setup>
+import { defineOptions } from 'vue'
+
+defineOptions({ name: 'TextField' })
+
+</script>
+
+<style scoped>
+
+</style>
